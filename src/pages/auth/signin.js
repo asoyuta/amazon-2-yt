6a74f9e2a91d6f47a94a5451d8edd19a33bbd456
asoyuta <asoyuta@icloud.com>
2021-12-22 16:55:37 +0900

    Day3

--- src/pages/auth/signin.js
+++ src/pages/auth/signin.js
@@ -3,13 +3,13 @@ import Header from '../../components/Header'
 
 function signIn({ providers }) {
   return (
-    <>
+    <div>
       <Header />
 
-      <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-56 px-14 text-center">
+      <div className="flex flex-col items-center min-h-screen py-2 px-14 text-center">
         <img className="bg-amazon_blue w-80 p-2" src="https://links.papareact.com/f90" alt="" />
         <p className="font-xs italic">This is not a REAL app, it is built for practice only</p>
-        <div className="mt-40">
+        <div className="mt-10">
           {Object.values(providers).map((provider) => (
             <div key={provider.name}>
               <button
@@ -22,7 +22,7 @@ function signIn({ providers }) {
           ))}
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
