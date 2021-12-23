fcccc573b60d083d09699f9eb5650012a2650c73
asoyuta <asoyuta@icloud.com>
2021-12-24 00:57:47 +0900

    Day 4 build

--- src/pages/orders.js
+++ src/pages/orders.js
@@ -6,39 +6,33 @@ import { collection, getDocs, orderBy, query } from 'firebase/firestore'
 import Order from '../components/Order'
 
 function Orders({ orders }) {
-  console.log(orders)
+  const { data: session } = useSession()
 
-  return <div></div>
+  return (
+    <div>
+      <Header />
+      <main className="max-w-screen-lg mx-auto p-10">
+        <h1 className="text-3xl border-b mb-2 pb-1 border-yellow-400">Your Orders</h1>
+
+        {session ? <h2>{orders.length} Orders</h2> : <h2>Please sign in to see your orders</h2>}
+
+        <div className="mt-5 space-y-4">
+          {orders?.map(({ id, amount, amountShipping, items, timestamp, images }) => (
+            <Order
+              key={id}
+              id={id}
+              amount={amount}
+              amountShipping={amountShipping}
+              items={items}
+              timestamp={timestamp}
+              images={images}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
-
-// function Orders({ orders }) {
-//   const { data: session } = useSession()
-
-//   return (
-//     <div>
-//       <Header />
-//       <main className="max-w-screen-lg mx-auto p-10">
-//         <h1 className="text-3xl border-b mb-2 pb-1 border-yellow-400">Your Orders</h1>
-
-//         {session ? <h2>{orders.length} Orders</h2> : <h2>Please sign in to see your orders</h2>}
-
-//         <div className="mt-5 space-y-4">
-//           {orders?.map(({ id, amount, amountShipping, items, timestamp, images }) => (
-//             <Order
-//               key={id}
-//               id={id}
-//               amount={amount}
-//               amountShipping={amountShipping}
-//               items={items}
-//               timestamp={timestamp}
-//               images={images}
-//             />
-//           ))}
-//         </div>
-//       </main>
-//     </div>
-//   )
-// }
 
 export default Orders
 
