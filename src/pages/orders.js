f717c4b393c051a11075afe6984571d9a02fb0fd
asoyuta <asoyuta@icloud.com>
2021-12-24 13:03:07 +0900

    Day 4 build

--- src/pages/orders.js
+++ src/pages/orders.js
@@ -6,6 +6,7 @@ import { collection, getDocs, orderBy, query } from 'firebase/firestore'
 import Order from '../components/Order'
 
 function Orders({ orders }) {
+  console.log(orders)
   const { data: session } = useSession()
 
   return (
@@ -14,7 +15,7 @@ function Orders({ orders }) {
       <main className="max-w-screen-lg mx-auto p-10">
         <h1 className="text-3xl border-b mb-2 pb-1 border-yellow-400">Your Orders</h1>
 
-        {session ? <h2>{orders.length} Orders</h2> : <h2>Please sign in to see your orders</h2>}
+        {/* {session ? <h2>{orders.length} Orders</h2> : <h2>Please sign in to see your orders</h2>}
 
         <div className="mt-5 space-y-4">
           {orders?.map(({ id, amount, amountShipping, items, timestamp, images }) => (
@@ -28,7 +29,7 @@ function Orders({ orders }) {
               images={images}
             />
           ))}
-        </div>
+        </div> */}
       </main>
     </div>
   )
@@ -53,31 +54,27 @@ export async function getServerSideProps(context) {
     const stripeOrders = await getDocs(
       query(collection(db, `users/${session.user.email}/orders`), orderBy('timestamp', 'desc'))
     )
+  } catch (error) {
+    console.log(err.message)
+  }
 
-    // Stripe orders
-    const orders = await Promise.all(
-      stripeOrders.docs.map(async (order) => ({
-        id: order.id,
-        amount: order.data().amount,
-        amountShipping: order.data().amount_shipping,
-        images: order.data().images,
-        timestamp: moment(order.data().timestamp.toDate()).unix(),
-        items: (
-          await stripe.checkout.sessions.listLineItems(order.id, {
-            limit: 100,
-          })
-        ).data,
-      }))
-    )
-    return {
-      props: {
-        orders,
-      },
-    }
-  } catch (err) {
-    console.log('ERROR', err.message)
-    return {
-      props: {},
-    }
+  // Stripe orders
+  // const orders = await Promise.all(
+  //   stripeOrders.docs.map(async (order) => ({
+  //     id: order.id,
+  //     amount: order.data().amount,
+  //     amountShipping: order.data().amount_shipping,
+  //     images: order.data().images,
+  //     timestamp: moment(order.data().timestamp.toDate()).unix(),
+  //     items: (
+  //       await stripe.checkout.sessions.listLineItems(order.id, {
+  //         limit: 100,
+  //       })
+  //     ).data,
+  //   }))
+  // )
+
+  return {
+    props: {},
   }
 }
