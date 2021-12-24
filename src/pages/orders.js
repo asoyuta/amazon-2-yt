a107b205d369095a4613ca56f3f92c4abf35afbf
asoyuta <asoyuta@icloud.com>
2021-12-24 12:45:07 +0900

    Day 4 build

--- src/pages/orders.js
+++ src/pages/orders.js
@@ -15,7 +15,7 @@ function Orders({ orders }) {
       <main className="max-w-screen-lg mx-auto p-10">
         <h1 className="text-3xl border-b mb-2 pb-1 border-yellow-400">Your Orders</h1>
 
-        {session ? <h2>{orders.length} Orders</h2> : <h2>Please sign in to see your orders</h2>}
+        {/* {session ? <h2>{orders.length} Orders</h2> : <h2>Please sign in to see your orders</h2>}
 
         <div className="mt-5 space-y-4">
           {orders?.map(({ id, amount, amountShipping, items, timestamp, images }) => (
@@ -29,7 +29,7 @@ function Orders({ orders }) {
               images={images}
             />
           ))}
-        </div>
+        </div> */}
       </main>
     </div>
   )
@@ -71,8 +71,6 @@ export async function getServerSideProps(context) {
   )
 
   return {
-    props: {
-      orders,
-    },
+    props: {},
   }
 }
