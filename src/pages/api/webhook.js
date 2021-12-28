6f1451e61c81e46e480470f886ef8d0bc77b5505
asoyuta <asoyuta@icloud.com>
2021-12-28 18:40:35 +0900

    initialized firebase admin

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -10,8 +10,6 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const endpointSecret = process.env.STRIPE_SIGNING_SECRET
 
 const fulfillOrder = async (session) => {
-  // console.log('Fulfilling order', session)
-
   return app
     .firestore()
     .collection('users')
